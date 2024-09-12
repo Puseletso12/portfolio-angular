e22cb425b33104ecfcb28711d34d1ac346008de8
Puseletso Chuene <puseletsochuene88@gmail.com>
2024-09-12 11:47:39 +0200

    images

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
@@ -7,6 +7,16 @@ import { Component } from '@angular/core';
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
+  public contactInfo = {
+    title: "Contact Me ☎️",
+    subtitle: "Discuss a project or just want to say hi? My Inbox is open for all.",
+    number: "+27 814288187",
+    email_address: "puseletsochuene88@gmail.com"
+  };
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 
 }
