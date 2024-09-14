08f197b6387004132faa69979df7d1c6822b12a0
Puseletso Chuene <puseletsochuene88@gmail.com>
2024-09-14 23:13:27 +0200

    portfolio page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,17 +3,11 @@ import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { PorfolioComponent } from './components/porfolio/porfolio.component';
 import { ContactComponent } from './components/contact/contact.component';
-import { HeaderComponent } from './components/header/header.component';
-import { ButtonComponent } from './components/button/button.component';
-import { EducationComponent } from './components/education/education.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'header', component: HeaderComponent },
   { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'portfolio', component: PorfolioComponent },
   { path: 'contact', component: ContactComponent },
-  { path: 'button', component: ButtonComponent },
-  { path: 'education', component: EducationComponent },
 ];
